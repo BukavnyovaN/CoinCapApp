9d7aa7ef61376487345d9d0f656bebed5732ba7f
Nadzeya Bukauniova <bukavneva99@mail.ru>
2023-08-26 19:18:05 +0300

    add configs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { trpc } from './utils/trpc';
 
 import { Main, Currency, NotFound } from './pages';
 import { PATHS } from './constants/paths';
 import { Layout } from './layout/Layout';
 import { useOverflow } from './hooks/useOverflow';
+import { configs } from "./configs";
 
 
 function App() {
@@ -15,8 +16,7 @@ function App() {
   const [trpcClient] = useState(() => trpc.createClient({
     links: [
       httpBatchLink({
-        // url: 'https://coin-cap-server.onrender.com/trpc',
-        url: 'http://127.0.0.1:4000/trpc',
+        url: configs.http.serverUrl
       })
     ]
   }))
@@ -29,11 +29,11 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <Routes>
-            <Route path={MAIN} element={<Layout />}>
-            <Route index element={<Main />} />
-            <Route path={CURRENCY} element={<Currency />} />
-            <Route path={NOT_FOUND} element={<NotFound />} />
-            <Route path={ANY} element={<NotFound />} />
+            <Route path={MAIN} element={<Layout/>}>
+              <Route index element={<Main/>}/>
+              <Route path={CURRENCY} element={<Currency/>}/>
+              <Route path={NOT_FOUND} element={<NotFound/>}/>
+              <Route path={ANY} element={<NotFound/>}/>
             </Route>
           </Routes>
         </BrowserRouter>
@@ -41,6 +41,6 @@ function App() {
     </trpc.Provider>
 
   );
-};
+}
 
 export { App };
---
+++ client/src/configs.ts
@@ -0,0 +1,5 @@
+export const configs = {
+  http: {
+    serverUrl: process.env.SERVER_URL || 'https://coin-cap-server.onrender.com/trpc',
+  }
+}
